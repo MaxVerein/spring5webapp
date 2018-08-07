153b71c50e1129c0063c77d1670d0a249b9f968d
MaxVerein <maxverein@mail.ru>
2018-08-07 22:16:38 +0300

    Commit2

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,30 +2,40 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
     }
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         setAuthorRepository(authorRepository);
         setBookRepository(bookRepository);
+        setPublisherRepository(publisherRepository);
     }
 
     private void initData(){
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+        publisherRepository.save(publisher);
+
         //Eric
         Author eric = new Author("Eric","Evans");
-        Book ddd = new Book("Domain Draiven Design", "1234","Harper Collins");
+        Book ddd = new Book("Domain Draiven Design", "1234",publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -34,10 +44,10 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Jonson");
-        Book noEJB = new Book("J2EE Development without EJB","23444","Worx");
-        rod.getBooks().add(noEJB);
-
+        Book noEJB = new Book("J2EE Development without EJB","23444",publisher);
         authorRepository.save(rod);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
         bookRepository.save(noEJB);
     }
 
@@ -55,5 +65,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public PublisherRepository getPublisherRepository() {
+        return publisherRepository;
+    }
+
+    public void setPublisherRepository(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
     }
 }
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -10,23 +10,25 @@ public class Book {
     private long Id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"),
     inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors = new HashSet<>();
 
-    public Book() {//
+    public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.setTitle(title);
         this.setIsbn(isbn);
         this.setPublisher(publisher);
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.setTitle(title);
         this.setIsbn(isbn);
         this.setPublisher(publisher);
@@ -50,11 +52,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
